735ba54f4ef519ed0d2f44bc40cfcef539089b78
djibi2607 <djibi26072018@gmail.com>
2026-04-27 15:38:14 -0400

    added the withdrawal email notification

--- app/utils/Resend.py
+++ app/utils/Resend.py
@@ -1,6 +1,7 @@
 import os, resend
 from typing import Dict
 from decimal import Decimal
+
 resend.api_key = os.getenv("RESEND_API_KEY")
 if not resend.api_key: 
     raise ValueError ("Resend key not found")
@@ -20,8 +21,22 @@ def sendWelcomeEmail(name: str) -> Dict:
     response: resend.Emails.SendResponse = resend.Emails.send(params)
     return response  
 
+def sendWelcomeEmail(name: str) -> Dict:
+    params: resend.Emails.SendParams = {
+        "from": "Acme <onboarding@resend.dev>",
+        "to": ["djibi26072018@gmail.com"],
+        "subject": "Account Creation",
+        "html": f"""
+            <h1>Welcome to Gentech, {name}!</h1>
+            <p>Your account has been successfully created.</p>
+            <p>You can now login and start making transactions.</p>
+            <br/>
+            <p>The Gentech Team</p>"""
+    }
+    response: resend.Emails.SendResponse = resend.Emails.send(params)
+    return response  
 
-def sendDepositEmail (name: str, amount : Decimal) -> Dict:
+def send sendDepositEmail(name:str, amount: Decimal)
     params: resend.Emails.SendParams = {
         "from" : "Acme <onboarding@resend.dev",
         "to" : ["djibi26072018@gmail.com"],
@@ -29,6 +44,22 @@ def sendDepositEmail (name: str, amount : Decimal) -> Dict:
          "html": f"""
             <h1>Your deposit has been successful, {name}!</h1>
             <p>Your deposit of {amount} is now available in your account.</p>
+            <br/>
+            <p>The Gentech Team</p>"""
+        }
+    
+    response : resend.Emails.SendParams = resend.Emails.send(params)
+
+    return response
+
+def send sendWithdrawEmail(name:str, amount: Decimal)
+    params: resend.Emails.SendParams = {
+        "from" : "Acme <onboarding@resend.dev",
+        "to" : ["djibi26072018@gmail.com"],
+        "subject" : "Withdraw Transaction",
+         "html": f"""
+            <h1>Your withdrawal has been successful, {name}!</h1>
+            <p>Your withdraw of {amount} has now been withdrawn from your account.</p>
             <br/>
             <p>The Gentech Team</p>"""
         }
